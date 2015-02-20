586ff8e6f916aaf61bc55b51682c8e786ce91569
p. Gomersbach <p.gomersbach@gmail.com>
2015-02-20 12:32:22 +0000

    add hostname

--- rely_ad/manifests/init.pp
+++ rely_ad/manifests/init.pp
@@ -1,6 +1,26 @@
-class rely_ad {
+class rely_ad (
+  $myhostname='testserver',
+  $domain='forest',
+  $domainname='jre.local',
+  $netbiosdomainname='jre',
+  $domainlevel='6',
+  $forestlevel='6',
+  $installtype='domain', # replica for second
+  $dsrmpassword='12_Changeme',
+  $localadminpassword='12_Changeme',
+){
 # wrapper class
 
+# set search domain
+# register nic connection in dns
+# use this connects sufix in dns registration
+# disable ipv6
+  # set hostname
+  if $myhostname != $::hostname {
+    notify { 'hostname change required': }
+  }
+
+  # install ad
   class {'windows_ad':
     install                => present,
     installmanagementtools => true,
@@ -8,17 +28,20 @@ class rely_ad {
     installflag            => true,
     configure              => present,
     configureflag          => true,
-    domain                 => 'forest',
-    domainname             => 'jre.local',
-    netbiosdomainname      => 'jre',
-    domainlevel            => '4',
-    forestlevel            => '4',
+    globalcatalog          => 'yes',
+    domain                 => $domain,
+    domainname             => $domainname,
+    netbiosdomainname      => $netbiosdomainname,
+    domainlevel            => $domainlevel,
+    forestlevel            => $forestlevel,
     databasepath           => 'c:\\windows\\ntds',
     logpath                => 'c:\\windows\\ntds',
     sysvolpath             => 'c:\\windows\\sysvol',
-    installtype            => 'domain',
-    dsrmpassword           => '12_Dwwmjenvgtn',
+    installtype            => $installtype,
+    dsrmpassword           => $dsrmpassword,
     installdns             => 'yes',
-    localadminpassword     => '12_Dwwmjenvgtn',
+    localadminpassword     => $localadminpassword,
   }
+# activate ad recyclebin
+# ptr enable
 }
