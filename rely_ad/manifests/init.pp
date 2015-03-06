21463b9d921493e3c6e1d5aa05e61d617789e284
p. Gomersbach <p.gomersbach@gmail.com>
2015-03-06 09:08:16 +0000

    change ifonly

--- rely_ad/manifests/init.pp
+++ rely_ad/manifests/init.pp
@@ -23,7 +23,7 @@ class rely_ad (
   exec {  'enable_dnsregistration':
     command  => '(Get-WmiObject Win32_NetworkAdapter -Filter "NetEnabled=True").GetRelated(\'Win32_NetworkAdapterConfiguration\').SetDynamicDNSRegistration($true,$false)',
     path     => $::path,
-    onlyif   => '(Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq "True"}).FullDNSRegistrationEnabled',
+    onlyif   => 'if ((Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq "True"}).FullDNSRegistrationEnabled) { exit 1 }',
     provider => powershell,
   }
 
