2b4e60bcfa751aa3725078dd390c017369861a85
p. Gomersbach <p.gomersbach@gmail.com>
2015-03-13 11:46:34 +0000

    add array

--- rely_ad/manifests/init.pp
+++ rely_ad/manifests/init.pp
@@ -20,7 +20,7 @@ class rely_ad (
   $namearray = split($domainname, '.')
   if  $forestlevel >= '4' {
     notify { "forestlevel $forestlevel detected, enable recycle bin, $namearray": }
-    notify { "Enable-ADOptionalFeature -Identity 'CN=Recycle Bin Feature,CN=Optional Features,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,DC=vkernel,DC=local' -Scope ForestOrConfigurationSet -Target 'vkernel.local'": }
+    notify { "$namearray": }
 #    exec {  'enable_ad_ recyclebin':
 #      command  => "Enable-ADOptionalFeature -Identity 'CN=Recycle Bin Feature,CN=Optional Features,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,DC=vkernel,DC=local' -Scope ForestOrConfigurationSet -Target \'$domainname\'",
 #      path     => $::path,
